d1baadf29b6133dca70d26b435d2bf326f30750b
Zeir Braidi <zeirbraidi@gmail.com>
2023-01-13 00:26:55 -0700

    Revert "week02"
    This reverts commit 68bf1ff3204cd3844cfaf80f12d41cf97af96c76.

--- common/nav.php
+++ common/nav.php
@@ -1,8 +1,13 @@
-<ul>
-    <li><a href="/phpmotors/home.php" title="View the PHP Motors home page">Home</a></li>
-    <li><a href="/phpmotors/cars/" title="View our Classics vehicles inventory">Classic</a></li>
-    <li><a href="/phpmotors/motors/" title="View our Sports vehicles inventory">Sports</a></li>
-    <li><a href="/phpmotors/trucks/" title="View our SUV vehicles inventory">SUV</a></li>
-    <li><a href="/phpmotors/used/" title="View our Trucks vehicles inventory">Trucks</a></li>
-    <li><a href="/phpmotors/rentals/" title="View our used inventory">Used</a></li>
-</ul>
+<nav>
+    <ul>
+        <li>
+            <a href="index.html">Home</a>
+        </li>
+        <li>
+            <a href="fresh.html">Fresh</a>
+        </li>
+        <li>
+            <a href="aboutus.html">About Us</a>
+        </li>
+    </ul>
+</nav>
--- home.php
+++ home.php
@@ -1,22 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <?php include $_SERVER['DOCUMENT_ROOT'].'/phpmotors/common/head.php'; ?>
-    </head>
-<body>
-<div id="wrapper">
-    <header>
-        <?php include $_SERVER['DOCUMENT_ROOT'] . "/phpmotors/common/header.php" ?>
-    </header>
-    <nav>
-        <?php include $_SERVER['DOCUMENT_ROOT'] . "/phpmotors/common/nav.php" ?>
-    </nav>
-    <main>
-        <?php include $_SERVER['DOCUMENT_ROOT']. "/phpmotors/common/main.php"?>
-    </main>
-    <footer>
-        <?php include $_SERVER['DOCUMENT_ROOT'] . "/phpmotors/common/footer.php" ?>
-    </footer>
-</div>
-</body>
-</html>
+<!-- header -->
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/common/header.php'; ?>
+<!-- main -->
+
+<!-- footer -->
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/common/footer.php'; ?>
